69cf92a1ceeeadd3e5e506e0d6e3c012f19bac2c
Ivan Topolnjak <ivantopo@gmail.com>
2016-11-11 03:40:38 +0100

    add scalactic validators.

--- copypaster/CopyPaster.scala
+++ copypaster/CopyPaster.scala
@@ -5,18 +5,19 @@ import scalax.file._
 object CopyPaster extends App {
   val validators = Map(
     "scalaz" -> "ScalazValidation.scala",
-    "cats" -> "CatsValidation.scala"
+    "cats" -> "CatsValidation.scala",
+    "scalactic" -> "ScalacticValidation.scala"
   )
 
   for ((validator, filename) <- validators) {
-    val root = Path("""d:/Code/validations-keep-us-sane/""" + validator, '/')
+    val root = Path("""/home/ivantopo/projects/hrscala/validations-keep-us-sane/""" + validator, '/')
 
     val source = root / filename
     val body = source.string
 
     (root / "src").deleteRecursively(force = true, continueOnFailure = false)
 
-    for (i <- 1 to 1500) {
+    for (i <- 1 to 500) {
       val bodyWithInjectedPackage = body.replaceFirst("(package hrscala.validation)", s"$$1\npackage copypasted$i")
       val target = root / "src" / "main" / "scala" / "hrscala" / "validation" / s"copypasted$i" / source.name
       target write bodyWithInjectedPackage
---
+++ scalactic/ScalacticValidation.scala
@@ -0,0 +1,58 @@
+package hrscala.validation
+
+import Models.{Row, ScalaDeveloper}
+import scala.util.Try
+import org.scalactic._
+import Accumulation._
+
+object ScalacticValidation
+    extends App
+    with CommonScalacticValidations
+    with BusinessScalacticValidations {
+
+  def constructScalaDeveloper(row: Row): ScalaDeveloper Or Every[ErrorMessage] = {
+    val name      = nonEmptyString(row.cells(0))
+    val age       = positiveNumber(row.cells(1)) when(noMinor)
+    val languages = commaSeparatedStrings(row.cells(2)) when mustHaveScala
+
+    withGood(name, age, languages) {
+      ScalaDeveloper(_, _, _)
+    }
+  }
+
+  Models.people foreach { row =>
+    println(constructScalaDeveloper(row))
+  }
+
+}
+
+
+trait CommonScalacticValidations {
+
+  def nonEmptyString(input: String): String Or One[ErrorMessage] =
+    if (input != null && input.nonEmpty) Good(input) else Bad(One("Input string is empty"))
+
+  def number(input: String): Int Or One[ErrorMessage] =
+    nonEmptyString(input) flatMap { nes =>
+      Try(Good(input.toInt)).getOrElse(Bad(One(s"Invalid number format for input: $input")))
+    }
+
+  def positiveNumber(input: String): Int Or One[ErrorMessage] =
+    number(input) flatMap { num =>
+      if(num > 0L) Good(num) else Bad(One("The input value is not positive"))
+    }
+
+  def commaSeparatedStrings(input: String): Seq[String] Or One[ErrorMessage] =
+    nonEmptyString(input) flatMap { nes =>
+      Good(nes.split(", *").toSeq)
+    }
+}
+
+trait BusinessScalacticValidations {
+
+  def noMinor(age: Int): Validation[ErrorMessage] =
+    if (age < 18) Fail("Person is a minor") else Pass
+
+  def mustHaveScala(languages: Seq[String]): Validation[ErrorMessage] =
+    if(languages.find(_ == "scala").isDefined) Pass else Fail("Languages did not contain Scala")
+}
